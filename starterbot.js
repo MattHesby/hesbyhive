f4e569d139238ff3c203e6534949989edcb3c2bb
Matt Hesby <mhesby@summitps.org>
2019-04-15 23:36:09 +0000

    updated starter and test

--- starterbot.js
+++ starterbot.js
@@ -1,9 +1,11 @@
 var bot = require("./bot"); //Don't change this
-bot.hostURL = 'http://aichallenge-gorgamite.c9users.io'; //Put the server url/IP adress here!
+bot.hostURL = 'http://SERVERWORKSPACE-SERVERUSERNAME.c9users.io'; //Put the server url/IP adress here!
 bot.key = "BOTKEYHERE"; //Set your bot key to this string!
 /***************************************************/
 //Write your code in this function!!!
 bot.direction = function(game) {
+    
+    /* ~~~ Determines and Organizes Data About The Game ~~~ */
     var enemyBots = [];
     var enemyBases = [];
     var myDir = "none";
@@ -17,15 +19,17 @@ bot.direction = function(game) {
         }
     }
 
-//This code decides what to do
-var task = "none"
+    /* ~~ This code decides what to do ~~ */
+    var task = "none"
 
-//This code decides how to do it
-if(task == "none"){
-    console.log("Going random!")
-    myDir = dirs[Math.floor(Math.random() * 4)];
-}
-    
+
+    /* ~~This code decides how to do it ~~ */
+    if (task == "none") {
+        console.log("Going random!")
+        myDir = dirs[Math.floor(Math.random() * 4)];
+    }
+
+
     return myDir;
 } //DO NOT CHANGE ANYTHING BELOW THIS LINE
 bot();
